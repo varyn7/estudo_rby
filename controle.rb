5359238d1da68f519b66839a3ce89f05fc4f79a0
varyn7 <joaojaimesampaio7@gmail.com>
2026-03-04 10:16:50 -0300

    Add files via upload
    estudo sobre as estruturas de controle else, is e elsif em ruby

---
+++ controle.rb
@@ -0,0 +1,16 @@
+#estrutura de controle if,else e elsif
+
+day = "sunday"
+
+
+if day == "sunday"
+  lunch = "crazy"
+elsif day == "holiday"
+  lunch = "tarde"
+else
+  lunch = "normal"
+end
+
+puts "lunch is #{lunch} #{day}"
+
+
