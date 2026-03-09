aab0ce7a59ac917f04d4b40a360174ea845ccb52
varyn7 <joaojaimesampaio7@gmail.com>
2026-03-09 14:18:27 -0300

    Add files via upload
    exercicio do conteudo de collection

---
+++ exercisescolletion.rb
@@ -0,0 +1,22 @@
+﻿#exercicio de potenciaÃ§Ã£o usando collections
+array=[1,2,3]
+puts "\n executando o .map e tranformando em potencia"
+#o \n faz uma uma nova linha no codigo
+new_array= array.map do |array|
+  array**2
+  array**2
+  array**2
+
+end
+puts new_array
+
+#exercicio de hash
+numeros = {a:1,b:2,c:3}
+numeros.each do |key, value|
+  puts "#{key} : #{value}"
+  puts "uma das chaves  \u00e9 #{key} e seu valor \u00e9 #{value}"
+
+end
+
+
+
