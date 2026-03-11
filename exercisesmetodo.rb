7b931a66f7dee659d387d5d65eb1baab49fa8138
varyn7 <joaojaimesampaio7@gmail.com>
2026-03-11 11:39:03 -0300

    Add files via upload
    exercicios de metodo usando gems no ruby

---
+++ exercisesmetodo.rb
@@ -0,0 +1,24 @@
+﻿# encoding: UTF-8
+
+def multiplicate(base, exp)
+  base ** exp
+end
+
+result = multiplicate(2, 5)
+puts "o resultado da potência é #{result}"
+
+#segundo exercicio
+
+
+require 'cpf_cnpj'
+def validation(cpf)
+  if CPF.valid?(cpf)
+    puts "cpf valido"
+  else
+    puts "cpf invalido"
+  end
+end
+
+print "enter your cpf number: "
+cpf_number = gets.to_s.strip
+validation(cpf_number)
